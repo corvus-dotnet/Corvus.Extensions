8fdec3a2de76e82a429284d9e99d09590136d486
Ed Freeman <Ed.Freeman@endjin.com>
2020-04-09 15:00:59 +0100

    Add golden path collection extension tests

---
+++ Solutions/Corvus.Extensions.Specs/CollectionExtensionsFeatures/CollectionExtensions.feature
@@ -0,0 +1,27 @@
+﻿Feature: CollectionExtensions
+	In order to manipulate collections
+	As a developer
+	I want extension methods
+
+Scenario Outline: Add a list to an existing list
+	Given I have a destination list containing <DestinationInitialContents>
+	And I have a source list containing <SourceInitialContents>
+	When I call AddRange on the destination list passing in the source list
+	Then the destination should now be <ExpectedResult>
+
+	Examples:
+	| DestinationInitialContents | SourceInitialContents | ExpectedResult   |
+	| 1, 2, 3                    | 4, 5, 6               | 1, 2, 3, 4, 5, 6 |
+	| 1, 2, 3                    |                       | 1, 2, 3          |
+	|                            | 4, 5, 6               | 4, 5, 6          |
+	|                            |                       |                  |
+	| 1, 2, 3                    | 2, 3, 4               | 1, 2, 3, 2, 3, 4 |
+
+
+
+# Multiple Target collection types
+# Empty vs non-empty initial target collections
+# Empty vs non-empty initial source collections
+# Source items overlap with target items
+# Null argument tests
+# Calling AddRange on itself
---
+++ Solutions/Corvus.Extensions.Specs/CollectionExtensionsFeatures/CollectionExtensionsSteps.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="CollectionExtensionsSteps.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Extensions.Specs.CollectionExtensionsFeatures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class CollectionExtensionsSteps
+    {
+        private ICollection<int>? destination;
+        private List<int>? source;
+
+        [Given(@"I have a destination list containing (.*)")]
+        public void GivenIHaveADestinationListContaining(List<int> listContents)
+        {
+            this.destination = listContents;
+        }
+
+        [Given(@"I have a source list containing (.*)")]
+        public void GivenIHaveASourceListContaining(List<int> listContents)
+        {
+            this.source = listContents;
+        }
+
+        [When(@"I call AddRange on the destination list passing in the source list")]
+        public void WhenICallAddRangeOnTheDestinationListPassingInTheSourceList()
+        {
+            this.destination!.AddRange(this.source!);
+        }
+
+        [Then(@"the destination should now be (.*)")]
+        public void ThenTheDestinationShouldNowBe(List<int> listContents)
+        {
+            Assert.IsTrue(Enumerable.SequenceEqual(this.destination, listContents));
+        }
+
+        [StepArgumentTransformation]
+        public List<int> TransformToListOfInt(string input)
+        {
+            return input.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(text => int.Parse(text)).ToList();
+        }
+    }
+}
