33ba25ad58aa4e7b0badb7ef466672f108ed6de8
James Dawson <james.dawson@endjin.com>
2023-01-20 18:10:37 +0000

    Testing multi logger support

--- build.ps1
+++ build.ps1
@@ -71,7 +71,7 @@ param (
     [string] $BuildModulePath,
 
     [Parameter()]
-    [version] $BuildModuleVersion = "1.2.2",
+    [version] $BuildModuleVersion = "1.2.3",
 
     [Parameter()]
     [version] $InvokeBuildModuleVersion = "5.7.1"
@@ -105,7 +105,7 @@ if ($MyInvocation.ScriptName -notlike '*Invoke-Build.ps1') {
 if (!($BuildModulePath)) {
     if (!(Get-Module -ListAvailable Endjin.RecommendedPractices.Build | ? { $_.Version -eq $BuildModuleVersion })) {
         Write-Information "Installing 'Endjin.RecommendedPractices.Build' module..."
-        Install-Module Endjin.RecommendedPractices.Build -RequiredVersion $BuildModuleVersion -Scope CurrentUser -Force -Repository PSGallery
+        Install-Module Endjin.RecommendedPractices.Build -RequiredVersion "1.2.3-beta0001" -Scope CurrentUser -Force -Repository PSGallery -AllowPrerelease
     }
     $BuildModulePath = "Endjin.RecommendedPractices.Build"
 }
